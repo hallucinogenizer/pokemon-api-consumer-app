bb4d3580344891fb01644a361e4789f9b9309b4c
Muhammad Rohan Hussain <rohanhussain1@yahoo.com>
2025-04-21 21:36:27 +0500

    added pokemon details page

---
+++ src/app/pokemon/[id]/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { usePokemonRetrieve } from "@/spec/pokemon/pokemon";
+import Link from "next/link";
+import { useParams } from "next/navigation";
+
+export default function PokemonDetailsPage() {
+  const { id } = useParams<{ id: string }>();
+  const { data } = usePokemonRetrieve(id);
+
+  return (
+    <div className="flex flex-col gap-6">
+      <Link href="/" className="text-primary underline" type="button">
+        Back
+      </Link>
+      <h1 className="text-3xl">{data?.data.name}</h1>
+      <main>
+        <img
+          src={data?.data.sprites.front_default}
+          width="200px"
+          height="200px"
+        />
+
+        <KeyValueDisplay keyName="Species" value={data?.data.species.name} />
+        <KeyValueDisplay
+          keyName="Base Stat"
+          value={String(data?.data.stats?.[0].base_stat)}
+        />
+        <KeyValueDisplay
+          keyName="Weight"
+          value={data?.data.weight?.toString()}
+        />
+      </main>
+    </div>
+  );
+}
+
+const KeyValueDisplay = ({
+  keyName,
+  value,
+}: {
+  keyName: string;
+  value: string | undefined;
+}) => (
+  <div className="flex items-center gap-2">
+    <strong>{keyName}:</strong>
+    <span>{value ?? "N/A"}</span>
+  </div>
+);
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,9 +1,15 @@
 import { PokemonSummary } from "@/spec/model";
+import Link from "next/link";
 
 export function PokemonCard({ pokemon }: { pokemon: PokemonSummary }) {
+  const splitPokemonUrl = pokemon.url.split("/");
+  const pokemonId = splitPokemonUrl[splitPokemonUrl.length - 2];
   return (
-    <div className="bg-gray-300 hover:bg-gray-200 cursor-pointer hover:-translate-y-1 transition-all p-6 border rounded-lg">
+    <Link
+      href={`/pokemon/${pokemonId}`}
+      className="bg-gray-300 hover:bg-gray-200 cursor-pointer hover:-translate-y-1 transition-all p-6 border rounded-lg"
+    >
       {pokemon.name}
-    </div>
+    </Link>
   );
 }
